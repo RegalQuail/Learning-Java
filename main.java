1e4d410e765733092d05c0b613db96ca4be1fa25
RegalQuail <82899321+RegalQuail@users.noreply.github.com>
2021-08-14 20:35:40 +0200

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,15 @@
+import java.util.Scanner;
 
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int amount = scanner.nextInt();
+
+        for (int months = 0; months < 3; months++) {
+            int payment = amount / 10;
+            amount -= payment;
+        }
+        System.out.println(amount);
+    }
+}
