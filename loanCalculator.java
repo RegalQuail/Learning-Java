b1531c17f76aa8b691ed3f37f07462284703fd09
RegalQuail <82899321+RegalQuail@users.noreply.github.com>
2021-08-15 12:25:33 +0200

    Create loanCalculator.java

---
+++ loanCalculator.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class loanCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int amount = scanner.nextInt();
+
+        for (int months = 0; months < 3; months++) {
+            int payment = amount / 10;
+            amount -= payment;
+        }
+        System.out.println(amount);
+    }
+}
