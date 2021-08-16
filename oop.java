9e8097c82905511ce72789e5f5cbde8c5c9a2e14
RegalQuail <82899321+RegalQuail@users.noreply.github.com>
2021-08-16 18:26:14 +0200

    Create oop.java

---
+++ oop.java
@@ -0,0 +1,32 @@
+class oop {
+    // Simply prints "Hello World !"
+    static void sayHello() {
+        System.out.println("Hello World !");
+    }
+
+    // Returns an integer value of 5
+    static int returnFive() {
+        return 5;
+    }
+    // Has a parameter
+    static void sayHelloTo(String name) {
+        System.out.println("Hello " + name);
+    }
+    // Evaluates the greater integer
+    static int max(int a, int b) {
+        if (a > b) {
+            return a;
+        }
+        else {
+            return b;
+        }
+    }
+    public static void main(String[] args) {
+        returnFive();
+        sayHello();
+        sayHelloTo("Jonah");
+        int result = max(7, 42);
+        System.out.println(result);
+    }
+    
+}
