e4aa93505c2926eeac21b6285747479fa6174349
RegalQuail <82899321+RegalQuail@users.noreply.github.com>
2021-08-17 22:49:10 +0200

    My first objects

---
+++ oop-folder/MyClass.java
@@ -0,0 +1,11 @@
+class MyClass {
+    public static void main(String[] args) {
+        Animal dog = new Animal();
+        dog.bark();
+
+        Vehicle v1 = new Vehicle();
+        Vehicle v2 = new Vehicle();
+        v1.colour = "Red";
+        v2.horn();
+    }
+}
---
+++ oop-folder/Vehicle.java
@@ -0,0 +1,10 @@
+public class Vehicle {
+    int maxSpeed;
+    int wheels;
+    String colour;
+    Double fuelCapacity;
+
+    void horn() {
+        System.out.println("Beep !");
+    }
+}
