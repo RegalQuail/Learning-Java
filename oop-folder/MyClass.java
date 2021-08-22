e108bc1c62da07fdef660a08d5e6c35bb7491f7e
RegalQuail <82899321+RegalQuail@users.noreply.github.com>
2021-08-22 14:06:30 +0200

    oop learning

--- oop-folder/MyClass.java
+++ oop-folder/MyClass.java
@@ -1,11 +1,16 @@
 class MyClass {
+    
     public static void main(String[] args) {
+        
         Animal dog = new Animal();
         dog.bark();
 
         Vehicle v1 = new Vehicle();
         Vehicle v2 = new Vehicle();
-        v1.colour = "Red";
+        v1.setColor("Red");
+        System.out.println(v1.getColor());
         v2.horn();
+
     }
+
 }
--- oop-folder/Vehicle.java
+++ oop-folder/Vehicle.java
@@ -1,4 +1,5 @@
 public class Vehicle {
+
     int maxSpeed;
     int wheels;
     String colour;
@@ -7,4 +8,25 @@ public class Vehicle {
     void horn() {
         System.out.println("Beep !");
     }
+
+    private String color;
+
+    // The getter method returns the value of the attribute
+    public String getColor() {
+        return color;
+    }
+
+    // The setter method takes a parameter and assigns it to the attribute
+    public void setColor(String c) {
+        this.color = c;
+    }
+
+    Vehicle() {
+        this.setColor("Red");
+    }
+
+    Vehicle(String c) {
+        this.setColor(c);
+    }
+
 }
