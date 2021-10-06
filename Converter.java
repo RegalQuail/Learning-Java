4c4222a91e972da184ef3853427619ac257ec5cc
RegalQuail <82899321+RegalQuail@users.noreply.github.com>
2021-10-06 22:32:17 +0200

    Solved

--- Converter.java
+++ Converter.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
 
 class Converter {
-    public static toBinary() {
+
+    public static String toBinary(int num) {
         String binary = "";
         while (num > 0) {
             binary = (num % 2) + binary;
             num /= 2;
         }
+        return binary;
     }
-    public static class Program {
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            int x = sc.nextInt();
-            System.out.println(Converter.toBinary(x));
-        }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println(Converter.toBinary(x));
     }
 }
---
+++ classes.java/classes/java/encapsulation.java
@@ -0,0 +1,25 @@
+
+class BankAccount {
+    private double balance = 0; // private hides the balance variable
+    public void deposit(double x) { // enabling access to it through the "deposit" method
+        if (x > 0) {
+            balance += x;
+        }
+    }
+}
+
+class Person {
+    private int age;
+    public void setAge(int age) {
+        if (age > 0) {
+            this.age = age;
+        }
+    }
+}
+
+/* 
+    Encapsulation provides:
+        - Control of the way data is accessed or modified
+        - More flexible and easily changed code
+        - Ability to change one part of the code without affecting other parts
+*/
