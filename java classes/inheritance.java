1d3806bebb6e3d3e755cad1a2a23d4e5e1c3b419
RegalQuail <82899321+RegalQuail@users.noreply.github.com>
2021-10-07 10:05:58 +0200

    More on classes

---
+++ java classes/inheritance.java
@@ -0,0 +1,25 @@
+class Animal {
+    protected int legs; // "protected": access modifier which makes the members only visible to the subclasses
+    public void eat() {
+        System.out.println("The animal eats.");
+    }
+
+    public void makeSound() {
+        System.out.println("Hi !");
+    }
+}
+
+class Dog extends Animal { // the Dog class inherits the legs variable from the Animal class
+    Dog() {
+        legs = 4;
+    }
+}
+// We can now declare a Dog object and call the "eat" method of its superclass:
+class MyClass {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eat();
+        dog.makeSound();
+    }
+}
+// Private methods are not inherited from the super class
---
+++ java classes/polymorphism.java
@@ -0,0 +1,36 @@
+// Polymorphism = having many forms
+// it occurs when there is a hierarchy of classes related to each other through inheritance
+// Polymorphism is one method, with different implementation
+class Animal {
+    public void makeSound() {
+        System.out.println("Grr...");
+    }
+    // as both Cat and Dog are Animal objects, we can do the following in main:
+    public static void main(String[] args) {
+        Animal a = new Cat();
+        Animal b = new Dog();
+// We've created two reference variables of type Animal, and pointed them to the Cat and Dog objects.
+// Now, we can call the makeSound() methods.    
+        a.makeSound(); // the reference variable a has a Dog object, the makeSound() method of the Dog class can be called.
+        b.makeSound(); // the same applies for variable b
+    }
+}
+// inherits from the Animal class + has its own implementation of the makeSound() method
+class Cat extends Animal { 
+    @Override
+    public void makeSound() {
+        System.out.println("Meow...");
+    }
+}
+// inherits from the Animal class + has its own implementation of the makeSound() method
+class Dog extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("Woof");
+    }
+}
+/*
+    This demonstrates that you can use the Animal variable 
+    without actually knowing that it contains an object of the subclass.
+    This is very useful when you have multiple subclasses of the superclass.
+*/
